33677d23b4a8d6feb1025bafe06b6d138e7d2cff
Evgeniy Mogranov <jenamovieo@gmail.com>
2026-03-10 03:04:31 +0500

    add file access to test-video

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: "1mb"}));
 app.use(express.static(path.join(__dirname, "static")));
+app.use("/test-video", express.static(path.join(__dirname, "test-video")));
 
 
 app.use("/api/movies", createMoviesRouter());
